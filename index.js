d301d26e007cc281be1e1df92c8347fae5c2e5ea
GreatEmon <contact.emonpro@gmail.com>
2025-09-21 03:51:21 +0600

    v2

---
+++ index.js
@@ -0,0 +1,80 @@
+const express = require('express')
+var cors = require('cors')
+const dotenv = require('dotenv')
+dotenv.config()
+const app = express()
+const port = 3000
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ndbz4pp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+app.use(cors())
+app.use(express.json())
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+    // all route here
+    const listcollection = client.db("roomX").collection("rooms")
+
+
+
+    app.get('/', async (req, res) => {
+      const cursor = listcollection.find();
+      const data = await cursor.toArray()
+      res.send(data)
+    })
+
+    app.get('/home', async (req, res) => {
+      const cursor = listcollection.find({availability : true}).limit(6);
+      const data = await cursor.toArray()
+      res.send(data)
+    })
+
+    app.post('/add', (req, res) => {
+      const added = listcollection.insertOne(req.body)
+      
+    })
+
+    app.get('/details/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await listcollection.findOne(query);
+      res.send(result)
+    })
+
+    app.get('/mylist/:email', async (req, res) => {
+      const email = req.params.email;
+      const query = {userEmail: email}
+      const result = await listcollection.find(query).toArray();
+      res.send(result)
+    })
+
+
+
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
